4d2405003b3b8c0d651eb64613065301cd7fd321
harold1024 <744924498@qq.com>
2020-04-01 17:30:13 +0800

    搭建首页

--- src/main.js
+++ src/main.js
@@ -3,13 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// import store from './store'
+import ElementUI from 'element-ui'
+
+import 'element-ui/lib/theme-chalk/index.css'
+// import './icons' // icon
 
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
+
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
+// import Layout from '@/layout'
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [
+        // {
+        //       path: '/redirect',
+        //       component: Layout,
+        //       hidden: true,
+        //       children: [{
+        //           path: '/redirect/:path(.*)',
+        //           component: () =>
+        //               import ('@/views/redirect/index')
+        //       }]
+        //   },
+        {
+            path: '/',
+            // component: Layout,
+            redirect: '/home',
+            children: [{
+                path: 'home',
+                component: () =>
+                    import ('@/views/home/index'),
+                name: 'Home',
+                // meta: { title: 'Dashboard', icon: 'dashboard', affix: true }
+            }]
+        },
+    ]
 })
